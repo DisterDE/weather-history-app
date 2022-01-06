f805175cf89af46aad69ecace98e305c71365f0c
Dister <disterko@gmail.com>
2022-01-06 20:58:53 +0300

    small changes

--- src/main/kotlin/pro/sky/java/weatherhistoryapp/handler/HistoryHandler.kt
+++ src/main/kotlin/pro/sky/java/weatherhistoryapp/handler/HistoryHandler.kt
@@ -1,10 +1,8 @@
 package pro.sky.java.weatherhistoryapp.handler
 
-import kotlinx.coroutines.flow.first
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.*
 import org.springframework.web.reactive.function.server.ServerResponse.ok
-import pro.sky.java.weatherhistoryapp.domain.HistoryRecord
 import pro.sky.java.weatherhistoryapp.service.HistoryService
 
 @Component
@@ -20,8 +18,7 @@ class HistoryHandler(
     }
 
     suspend fun save(serverRequest: ServerRequest): ServerResponse {
-        return service.save(serverRequest.bodyToFlow<HistoryRecord>().first())
-            .run { ok().bodyValueAndAwait(this) }
+        return service.save(serverRequest.awaitBody()).run { ok().bodyValueAndAwait(this) }
     }
 
     suspend fun delete(serverRequest: ServerRequest): ServerResponse {
